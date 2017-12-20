1cf0da0d03759a1d8b3a007299f7688043ca2993
kxxoling <kxxoling@gmail.com>
2017-12-20 22:34:44 +0800

    Using tempfile module to create temp directory and files.

--- pixiv_ugoku.py
+++ pixiv_ugoku.py
@@ -2,19 +2,23 @@
 import os
 import re
 import json
+import logging
 import subprocess
 from zipfile import ZipFile
+from tempfile import mkstemp, mkdtemp
 
 import requests
 
 headers = {'Referer': 'https://www.pixiv.net/'}
 session = requests.Session()
 session.trust_env = True
+logger = logging.getLogger()
 
 re_ugoku_illust_data = r'pixiv\.context\.ugokuIllustData\s+= ([^;]+)'
 
 
 def get_html(url):
+    logger.info('Fetching HTML from %s' % url)
     return session.get(url).content
 
 
@@ -23,17 +27,17 @@ def parse_html(html):
         re_ugoku_illust_data, html.decode('utf-8')
     )
     assert ugoku_data
+    logger.debug('Got data: ```%s```' % ugoku_data)
     ugoku_json = json.loads(ugoku_data.groups()[0])
     return ugoku_json
 
 
 def download_zip(url):
     id_ = url.rsplit('/')[-1].rsplit('.')[0]
-    tmp_dir = os.path.join('/tmp', str(id_))
-    if not os.path.exists(tmp_dir):
-        os.mkdir(tmp_dir)
-    tmp_file = tmp_dir + '.zip'
+    tmp_dir = mkdtemp(prefix=str(id_))
+    _, tmp_file = mkstemp(prefix=str(id_), dir=tmp_dir)
 
+    logger.info('Saving zip to %s' % tmp_file)
     with open(tmp_file, 'wb') as f:
         for chunk in session.get(
             url, headers=headers
@@ -44,19 +48,19 @@ def download_zip(url):
     with ZipFile(tmp_file) as tar:
         for name in [i for i in tar.namelist() if i != 'index']:
             tar.extract(name, path=tmp_dir)
-    # os.remove(tmp_file)
-    return tmp_dir
+    return tmp_dir, tmp_file
 
 
-def save_ugoku(url):
+def save_ugoku(url, filename='pixiv'):
     html = get_html(url)
     ugoku_json = parse_html(html)
     ugoku_zip = ugoku_json['src']
-    dirname = download_zip(ugoku_zip)
+    dirname, _ = download_zip(ugoku_zip)
     fps = 1000 / 80
+
     subprocess.check_call(
         'ffmpeg -i "{dirname}/%6d.jpg" -y {output}'.format(
-            fps=fps, dirname=dirname, output='pixiv.mp4'
+            fps=fps, dirname=dirname, output='%s.mp4' % filename
         ),
         shell=True
     )
